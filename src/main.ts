876c56b8f9fdcfa00601fb6b1093ad33f4859d9a
lisandrovaldezz <lisandroval09@gmail.com>
2026-04-10 17:01:46 -0300

    modificar cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173', 'zawt-3phk.vercel.app'],
     credentials: true,
   });
   app.use(cookieParser());
