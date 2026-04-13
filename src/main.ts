0e6d1eb5174e2367cec5dc5ea64887b00a3c6512
Lisandro Valdez <71908634+lisandrovaldez@users.noreply.github.com>
2026-04-13 14:56:00 -0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:5173', 'zawt-3phk.vercel.app'],
+    origin: ['http://localhost:5173', 'https://zawt.vercel.app/'],
     credentials: true,
   });
   app.use(cookieParser());
